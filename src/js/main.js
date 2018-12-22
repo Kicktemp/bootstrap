e0b14c8ff3932439d11ecaa3fe6d2a9718224b11
Niels Nübel <n.nuebel@nn-medienagentur.de>
2018-12-22 22:54:19 +0100

    add eslint

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,1 @@
-alert('Niels')
+alert('Niels22')
--- webpack/webpack.config.base.babel.js
+++ webpack/webpack.config.base.babel.js
@@ -30,6 +30,21 @@ export default {
     path: utils.paths.PUBLIC_PATH
   },
   stats: 'none',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js)$/,
+        loader: 'eslint-loader',
+        options: {
+          configFile: ifProduction('./.eslintrc.js', './.eslintrc-dev.js'),
+          formatter: require('eslint-friendly-formatter')
+        },
+        exclude: /node_modules/,
+        include: utils.resolve(utils.kicktempConf.src.base)
+      }
+    ]
+  },
   plugins: utils.removeEmpty([
     new webpack.NamedModulesPlugin(),
     new WebpackBar(),
