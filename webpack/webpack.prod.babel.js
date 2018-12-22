d67418b3bbc9c16258b5739bca71a6b186eef995
Niels Nübel <n.nuebel@nn-medienagentur.de>
2018-12-22 13:54:11 +0100

    add output for production webpack

--- webpack/webpack.prod.babel.js
+++ webpack/webpack.prod.babel.js
@@ -3,6 +3,11 @@ const utils = require('./utils')
 const baseWebpackConfig = require('./webpack.config.base.babel.js')
 
 const prodWebpackConfig = merge(baseWebpackConfig.default, {
+  output: {
+    publicPath: '/',
+    filename: utils.assetsPath('js/[name].js'),
+    chunkFilename: utils.assetsPath('js/chunks/[name].js')
+  },
 })
 
 module.exports = prodWebpackConfig
